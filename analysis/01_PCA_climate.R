22091719444dfb439a782e0bd764da4ad9696223
Selina Baldauf <selina.baldauf@gmx.de>
2025-01-20 14:14:04 +0100

    Auto-format code

--- analysis/01_PCA_climate.R
+++ analysis/01_PCA_climate.R
@@ -1,4 +1,4 @@
-# Purpose: PCA analysis for extracting compound variable of climate gradient 
+# Purpose: PCA analysis for extracting compound variable of climate gradient
 #                       (increasing precipitation and decreasing temperature)
 
 # packages -----
@@ -10,8 +10,8 @@ header_data <- read_csv("data/Environm_variabl.csv")
 
 # selected climate variables
 climate <- header_data %>%
-  group_by(series) %>% 
-  summarise(BIO1=mean(BIO1), BIO12=mean(BIO12)) %>% 
+  group_by(series) %>%
+  summarise(BIO1 = mean(BIO1), BIO12 = mean(BIO12)) %>%
   mutate(
     Temprt = as.vector(scale(BIO1, scale = TRUE)),
     Precipt = as.vector(scale(BIO12, scale = TRUE))
@@ -30,13 +30,13 @@ climate_pca
 summary(climate_pca)
 
 # Scree plot
-fviz_eig(climate_pca, 
-         addlabels = TRUE
-         )
+fviz_eig(climate_pca,
+  addlabels = TRUE
+)
 
 # 2-dimension plot of PCA coordinates
 fviz_pca_var(climate_pca,
- # axes = c(1, 2),
+  # axes = c(1, 2),
   col.var = "contrib", # Color by contributions to the PC
   gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
   repel = TRUE # Avoid text overlapping
@@ -57,7 +57,7 @@ climate_pca_ind$coord
 
 pca1_clima <- climate_pca_ind$coord[, 1] # select 1st PC
 
-# join with raw data 
+# join with raw data
 climate <- bind_cols(climate, pca1_clima = pca1_clima)
 
 
@@ -66,14 +66,14 @@ head(climate_pca_var$cor)
 
 # Plot correlations of variables with the PC1 and PC2
 library("corrplot")
-corrplot(climate_pca_var$cor, is.corr=F) 
+corrplot(climate_pca_var$cor, is.corr = F)
 
 ## Plot contribution of variables to the PC1 and PC2
 library(ggcorrplot)
 ggcorrplot(climate_pca_var$contrib,
-           hc.order = TRUE, # type = "lower",
-           lab = TRUE, lab_size = 3,
-           colors = c("red", "white", "blue")
+  hc.order = TRUE, # type = "lower",
+  lab = TRUE, lab_size = 3,
+  colors = c("red", "white", "blue")
 )
 
 climate_pca_var$cor
