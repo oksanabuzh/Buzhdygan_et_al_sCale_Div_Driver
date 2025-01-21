a93c72a613732910ab1b1bf1e28634d806269199
Selina Baldauf <selina.baldauf@fu-berlin.de>
2025-01-21 12:37:30 +0100

    Clean and comment script

--- analysis/07_aggregation.R
+++ analysis/07_aggregation.R
@@ -1,51 +1,62 @@
 # Purpose: LMM analysis for spatial aggregation as response variable
-# libraries----
+
+# Load libraries -----------------------------------------------------------
 library(tidyverse)
 library(car)
 library(lme4)
 library(lmerTest)
-library(ggplot2)
 library(sjPlot)
 library(performance)
 library(patchwork)
 
+# Define habitat colors to be used in all plots to distinguish habitats
+habitat_colors = c(
+  saline = "#4e3910",
+  complex = "#CC6600",
+  dry = "#e3c28b",
+  wet = "#CC99FF",
+  mesic = "#0066FF",
+  fringe = "#00B200",
+  alpine = "#006600")
 
-# prepare data----
+# Set theme for the model plots
+set_theme(base = theme_bw(),
+          axis.textsize.x = 1,
+          axis.textsize.y = 1,
+          axis.textcolor = "black",
+          axis.title.color = "black",
+          axis.title.size = 1.4,
+          legend.pos = "None",
+          geom.linetype = 2)
+
+
+# Read and prepare data -------------------------------------------------------
+
+# SR - species richness
+# ENSPIE - evenness measure calculated as inverse Simpson using species cover
+# cover - is cumulative plant cover
 
 # "data/alpha_beta_gamma_community_variabl.csv" combines all diversity measures and plant cover
 # alpha diversity measures (SR and ENSPIE) include doubled 10 m2 plots,
-## thus "series" (i.e. 100m2 plots), nested in dataset (separate vegetation survey campaign)
-## are fitted as a random effect
+# thus "series" (i.e. 100m2 plots), nested in dataset (separate vegetation survey campaign)
+# are fitted as a random effect
 # gamma diversity measures (SR and ENSPIE)include 100m2 plots (i.e. the sample size is half of what we have for the 10m2 plots)
 # beta diversity measures (SR and ENSPIE) are calculated as gamma/alpha
 
-## SR - species richness
-## ENSPIE - evenness measure calculated as inverse Simpson using species cover
-## cover - is cumulative plant cover
-
-
-# "data/climate_PCA.csv" contains scores for the compound climate variable,
-# derived from the PCA analysis in "1_prepare_data/ PCA_environment.R"
-
-# "data/Environm_variabl.csv" contains all environmental data
-
-
-aggregation <- read_csv("data/aggregation.csv")
-
+# Read climate data and compund climate variable from PCA analysis in "1_prepare_data/ PCA_environment.R"
 climate_PCA <- read.csv("data/climate_PCA.csv")
 
+# Read all environmental data
 header <- read_csv("data/Environm_variabl.csv") %>%
   full_join(
     read.csv("data/climate_PCA.csv"),
     by = "series"
   )
 
-str(header)
-names(header)
+# Read aggregation data
+aggregation <- read_csv("data/aggregation.csv")
 
-# Take mean across two subplots for the purpose of plotting:
-# plot on a mean alpha per series to omit pseudo-replication of the plots on the figure:
-
+# mean per series (per 100m2 plots)
 header_mean <- header %>%
   select(c(series, lat, lon, zonality, habitat_broad,
     where(is.numeric))) %>%
@@ -53,7 +64,7 @@ header_mean <- header %>%
   summarize(across(where(is.numeric), \(x) mean(x, na.rm = TRUE))) %>%
   ungroup()
 
-
+# Prepare subset of data for beta scale --------------------------------------
 beta_gamma <- read_csv("data/alpha_beta_gamma_community_variabl.csv") %>%
   filter(type == "gamma" | type == "beta") %>%
   unite("metric", c(type, scale, metric), sep = "_") %>%
@@ -61,13 +72,6 @@ beta_gamma <- read_csv("data/alpha_beta_gamma_community_variabl.csv") %>%
   full_join(header_mean, by = c("dataset", "series")) %>%
   mutate(dataset = factor(dataset))
 
-str(beta_gamma)
-names(beta_gamma)
-
-# dataset is a separate vegetation survey campaign
-beta_gamma$dataset
-
-## data beta----
 beta_data <- beta_gamma %>%
   dplyr::select(dataset, series, habitat_broad, zonality,
     gamma_100_div, gamma_100_ENSPIE, gamma_100_cover,
@@ -92,10 +96,10 @@ str(beta_data)
 summary(beta_data)
 
 
+# Check correlation between variables -----------------------------------------
 beta_data %>%
   dplyr::select(gamma_100_div, gamma_100_ENSPIE, gamma_100_cover, ,
     beta_100_div, beta_100_ENSPIE, aggregation) %>%
-  # rename(SR=sowndiv, "FG richness"=numfg) %>%
   cor() |>
   ggcorrplot::ggcorrplot(
     lab = TRUE, type = "lower",
@@ -103,14 +107,10 @@ beta_data %>%
     colors = c("red", "white", "blue")
   )
 
+# GLLM model of aggregation --------------------------------------------------
 
+# Exploration ----------------------------------------------------------------
 
-
-# GLLM----
-
-## Exploration----
-
-# Check the model:
 m <- lmer(aggregation ~
   poly(pca1_clima, 2) +
   poly(pH, 2) +
@@ -124,13 +124,10 @@ plot(m) # heteroscedasticity
 qqnorm(resid(m))
 qqline(resid(m))
 
-
-
 Anova(m)
 
-# Model 1: all predictors (except precipitation CV)----
+# Model 1: all predictors (except precipitation CV) ---------------------------
 # test quadratic effects of climate, soil C, pH, and litter
-# Model selection ----
 
 # poly(pca1_clima, 2)
 # poly(pH, 2) is marginal
@@ -149,14 +146,12 @@ m1_2 <- lmer(aggregation ~ pca1_clima +
   grazing_intencity + mowing +
   (1 | dataset), data = beta_data)
 
-
 m1_3 <- lmer(aggregation ~ poly(pca1_clima, 2) +
   pH +
   Corg_percent +
   cover_litter +
   grazing_intencity + mowing +
   (1 | dataset), data = beta_data)
-
 
 m1_4 <- lmer(aggregation ~
   pca1_clima +
@@ -166,19 +161,17 @@ m1_4 <- lmer(aggregation ~
   grazing_intencity + mowing +
   (1 | dataset), data = beta_data)
 
-
 # calculate and compare AIC
 AIC(m1_1, m1_2, m1_3, m1_4) %>%
   arrange(+AIC) %>%
   mutate(delta_AIC = AIC - min(AIC))
 
+# Check the best model
 Anova(m1_1)
 # Anova(m1_4_ENSPIE)
 
+# Model 2: Add Precipiation variability --------------------------------------
 
-# Model 2: Add Prec_Varieb ----
-
-### Model selection -----
 m2_1 <- lmer(aggregation ~
   poly(pca1_clima, 2) +
   poly(Prec_Varieb, 2) +
@@ -198,24 +191,24 @@ m2_2 <- lmer(aggregation ~
   grazing_intencity + mowing +
   (1 | dataset), data = beta_data)
 
-
 # calculate and compare AIC
 AIC(m2_1, m2_2) %>%
   arrange(+AIC) %>%
   mutate(delta_AIC = AIC - min(AIC))
 
-
+# Check the best model
 Anova(m2_1)
 
 
-
-### -> Additional analysis for precipitation CV effects  -----
+# Additional analysis for precipitation CV effects  ---------------------------
 # Analysis whether precipitation variability adds explanatory power beyond the
 # nonlinear effect of the climate gradient:
 # Model m3_1 includes both linear and quadratic terms for the climate gradient.
 # Model m3_2 includes the linear effects of both climate gradient and
-# precipitation variability (i.e., the quadratic term for the climate gradient was replaced by precipitation variability).
-# If the latter model is better (e.g. AIC is smaller than two units), we have more support for claim that the precipitation variability effect is shown.
+# precipitation variability (i.e., the quadratic term for the climate gradient 
+# was replaced by precipitation variability).
+# If the latter model is better (e.g. AIC is smaller than two units), we have more
+# support for claim that the precipitation variability effect is shown.
 
 m3_1 <- lmer(aggregation ~
   poly(pca1_clima, 2) +
@@ -234,86 +227,78 @@ AIC(m3_1, m3_2) %>%
 Anova(m3_1)
 Anova(m3_2)
 
-# Plots----
-#         saline    complex       dry       wet       mesic        fringe       alpine
-col = c("#4e3910", "#CC6600", "#e3c28b", "#CC99FF", "#0066FF", "#00B200", "#006600")
+# Make model plots -----------------------------------------------------------
 
-set_theme(base = theme_bw(), axis.textsize.x = 1, axis.textsize.y = 1, axis.textcolor = "black",
-  axis.title.color = "black", axis.title.size = 1.4, legend.pos = "None", geom.linetype = 2)
-
-# final models:
+# final models used in the plots:
 Anova(m1_1)
 Anova(m2_1)
 
 
-## Clima ----
+# Climate plot ---------------------------------------------------------------
 
 clima_pred_beta_aggr <- get_model_data(m1_1, type = "pred", terms = "pca1_clima[-1.2:4.8, by=.001]")
-clima_pred_beta_aggr
 
 Fig.betaaggr_clima <- ggplot(clima_pred_beta_aggr, aes(x, predicted)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.1) +
-  geom_point(data = beta_data, aes(pca1_clima, aggregation, fill = habitat, col = habitat), size = 1, alpha = 0.8, pch = 21) +
-  scale_fill_manual(values = col) + scale_color_manual(values = col) +
+  geom_point(data = beta_data, 
+             aes(pca1_clima, aggregation, fill = habitat, col = habitat), 
+             size = 1, alpha = 0.8, pch = 21) +
+  scale_fill_manual(values = habitat_colors) + 
+  scale_color_manual(values = habitat_colors) +
   labs(y = "Species aggregation", x = 'Climate gradient') +
-  geom_line(linetype = 1, size = 1, col = "black")
+  geom_line(linetype = 1, linewidth = 1, col = "black")
 
 Fig.betaaggr_clima
 
 
-plot_model(m1_1_aggr, type = "pred", terms = "pca1_clima[-1.2:4.8, by=.001]", #  show.data=F
-  title = "", line.size = 0.5) + aes(linetype = "solid") +
-  labs(y = expression(paste("beta ENS"[PIE])), x = 'Climate gradient (PC)') +
-  geom_point(data = beta_data, aes(pca1_clima, BRAY_tn, color = habitat), size = 3, alpha = 0.8) +
-  scale_color_manual(values = col) # +  labs(color='Habitat type')
-
-
-
-## Soil C ----
+# Soil C plot ----------------------------------------------------------------
 Humus_pred_beta_aggr <- get_model_data(m1_1, type = "pred", terms = "Corg_percent[0:9.5, by=.001]")
-Humus_pred_beta_aggr
 
 Fig.betaaggr_soilC <- ggplot(Humus_pred_beta_aggr, aes(x, predicted)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.1) +
-  geom_point(data = beta_data, aes(Corg_percent, aggregation, fill = habitat, col = habitat), size = 1, alpha = 0.8, pch = 21) +
-  scale_fill_manual(values = col) + scale_color_manual(values = col) +
+  geom_point(data = beta_data, 
+             aes(Corg_percent, aggregation, fill = habitat, col = habitat), 
+             size = 1, alpha = 0.8, pch = 21) +
+  scale_fill_manual(values = habitat_colors) + scale_color_manual(values = habitat_colors) +
   labs(y = "Species aggregation", x = 'Soil C') +
-  geom_line(linetype = 5, size = 0.5, col = "black")
+  geom_line(linetype = 5, linewidth = 0.5, col = "black")
 
 Fig.betaaggr_soilC
 
 
-## Litter % ----
+# Litter % plot --------------------------------------------------------------
 Litter_pred_beta_aggr <- get_model_data(m1_1, type = "pred", terms = "cover_litter[0:100, by=0.01]")
-Litter_pred_beta_aggr
 
 Fig.betaaggr_Litter <- ggplot(Litter_pred_beta_aggr, aes(x, predicted)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.1) +
-  geom_point(data = beta_data, aes(cover_litter, aggregation, fill = habitat, col = habitat), size = 1, alpha = 0.8, pch = 21) +
-  scale_fill_manual(values = col) + scale_color_manual(values = col) +
+  geom_point(data = beta_data, 
+             aes(cover_litter, aggregation, fill = habitat, col = habitat), 
+             size = 1, alpha = 0.8, pch = 21) +
+  scale_fill_manual(values = habitat_colors) + 
+  scale_color_manual(values = habitat_colors) +
   labs(y = "Species aggregation", x = 'Litter cover') +
-  geom_line(linetype = 1, size = 1, col = "black")
+  geom_line(linetype = 1, linewidth = 1, col = "black")
 
 Fig.betaaggr_Litter
 
-## Soil pH ----
+# Soil pH plot ---------------------------------------------------------------
 pH_pred_beta_aggr <- get_model_data(m1_1, type = "pred", terms = "pH[3.7:9, by=.001]")
-pH_pred_beta_aggr
 
 Fig.betaaggr_soil.pH <- ggplot(pH_pred_beta_aggr, aes(x, predicted)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.1) +
-  geom_point(data = beta_data, aes(pH, aggregation, fill = habitat, col = habitat), size = 1, alpha = 0.8, pch = 21) +
-  scale_fill_manual(values = col) + scale_color_manual(values = col) +
+  geom_point(data = beta_data, 
+             aes(pH, aggregation, fill = habitat, col = habitat), 
+             size = 1, alpha = 0.8, pch = 21) +
+  scale_fill_manual(values = habitat_colors) + 
+  scale_color_manual(values = habitat_colors) +
   labs(y = "Species aggregation", x = 'Soil pH') +
-  geom_line(linetype = 5, size = 0.5, col = "black")
-
+  geom_line(linetype = 5, linewidth = 0.5, col = "black")
 
 Fig.betaaggr_soil.pH
 
 
-## Grazing ----
+# Grazing plot --------------------------------------------------------------
 grazing_pred_beta_aggr <- get_model_data(m1_1, type = "pred", terms = "grazing_intencity[-0.2:3.2, by=0.01]")
-grazing_pred_beta_aggr
 
 Fig.betaaggr_grazing <- ggplot(grazing_pred_beta_aggr, aes(x, predicted)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.1) +
@@ -321,48 +306,41 @@ Fig.betaaggr_grazing <- ggplot(grazing_pred_beta_aggr, aes(x, predicted)) +
     fill = habitat, col = habitat),
   size = 1, alpha = 0.8, pch = 21,
   position = position_jitter(w = 0.2)) +
-  scale_fill_manual(values = col) + scale_color_manual(values = col) +
+  scale_fill_manual(values = habitat_colors) + 
+  scale_color_manual(values = habitat_colors) +
   labs(y = "Species aggregation", x = 'Grazing intencity') +
   geom_line(linetype = 5, size = 0.5, col = "black")
 
 Fig.betaaggr_grazing
 
-
-## Mowing ----
-
-plot_model(m1_1, type = "pred", terms = "mowing", show.data = T,
-  title = "", line.size = 0.5)
-
+# Mowing plot ---------------------------------------------------------------
 Fig.betaaggr_mowing <- ggplot(beta_data, aes(mowing, aggregation)) +
   geom_boxplot(color = "black") +
   labs(y = "Species aggregation", x = 'Mowing') +
-  geom_point(aes(color = habitat, fill = habitat), pch = 21, position = position_jitter(w = 0.1), size = 3, alpha = 0.8) +
-  scale_color_manual(values = col) + scale_fill_manual(values = col) +
+  geom_point(aes(color = habitat, fill = habitat), pch = 21, 
+             position = position_jitter(w = 0.1), size = 3, alpha = 0.8) +
+  scale_color_manual(values = habitat_colors) + 
+  scale_fill_manual(values = habitat_colors) +
   labs(color = 'Habitat type')
 
 Fig.betaaggr_mowing
 
-
-## Prec_Varieb ----
+# Precipitation variability plot -------------------------------------------
 precipCV_pred_beta_aggr <- get_model_data(m2_1, type = "pred", terms = "Prec_Varieb")
-precipCV_pred_beta_aggr
 
 Fig.betaaggr_precip.CV <- ggplot(precipCV_pred_beta_aggr, aes(x, predicted)) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.1) +
-  geom_point(data = beta_data, aes(Prec_Varieb, aggregation, fill = habitat, col = habitat), size = 1, alpha = 0.8, pch = 21) +
-  scale_fill_manual(values = col) + scale_color_manual(values = col) +
+  geom_point(data = beta_data, 
+             aes(Prec_Varieb, aggregation, fill = habitat, col = habitat), 
+             size = 1, alpha = 0.8, pch = 21) +
+  scale_fill_manual(values = habitat_colors) + 
+  scale_color_manual(values = habitat_colors) +
   labs(y = "Species aggregation", x = 'Precipitation CV') +
   geom_line(linetype = 5, size = 0.5, col = "black")
 
 Fig.betaaggr_precip.CV
 
-## Merge plots----
-
-library(patchwork)
-
-set_theme(base = theme_bw(), axis.textsize.x = 0.8, axis.textsize.y = 0.8, axis.textcolor = "black",
-  axis.title.color = "black", axis.title.size = 0.9, legend.pos = "None",
-  geom.linetype = 2)
+# Combine all plots ----------------------------------------------------------
 
 Fig.betaaggr_clima +
   Fig.betaaggr_precip.CV +
@@ -371,7 +349,7 @@ Fig.betaaggr_clima +
   Fig.betaaggr_Litter +
   Fig.betaaggr_grazing +
   plot_annotation(tag_levels = 'a') +
-  plot_layout(ncol = 3) & # ylab(NULL) &
-  theme( # plot.margin = margin(3, 1, 3, 20),
+  plot_layout(ncol = 3) & 
+  theme( 
     plot.tag = element_text(size = 11, face = 'bold'),
     plot.tag.position = c(0.25, 1.06))
