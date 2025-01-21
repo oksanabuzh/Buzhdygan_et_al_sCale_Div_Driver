99822420233ca3c22500fdd8000e5acc9c938b28
Selina Baldauf <selina.baldauf@fu-berlin.de>
2025-01-21 21:12:42 +0100

    Clean and comment

--- analysis/Fig.3b_R2.R
+++ analysis/Fig.3b_R2.R
@@ -1,12 +1,12 @@
-# Purpose: summertime and visualize R2
+# Purpose: Visualize R2 and produce Figures 3b
 # Fig 3 b ----
 
-# merge tables form the (G)LMMs outputs
 
 library(tidyverse)
 
-# partial R2
+# Read and prepare data -------------------------------------------------------
 
+# Read R2 of all models
 R2_alpha_SR <- read_csv("results/R2_alpha_SR.csv") %>% select(Effect, Rsq) %>% rename(alpha_SR = Rsq)
 R2_alpha_ENSPIE <- read_csv("results/R2_alpha_ENSPIE.csv") %>% select(Effect, Rsq) %>% rename(alpha_ENSPIE = Rsq)
 R2_gamma_SR <- read_csv("results/R2_gamma_SR.csv") %>% select(Effect, Rsq) %>% rename(gamma_SR = Rsq)
@@ -14,15 +14,16 @@ R2_gamma_ENSPIE <- read_csv("results/R2_gamma_ENSPIE.csv") %>% select(Effect, Rs
 R2_beta_SR <- read_csv("results/R2_beta_SR.csv") %>% select(Effect, Rsq) %>% rename(beta_SR = Rsq)
 R2_beta_ENSPIE <- read_csv("results/R2_beta_ENSPIE.csv") %>% select(Effect, Rsq) %>% rename(beta_ENSPIE = Rsq)
 
+# Join all R2 values by effect
+R2_part_all <- purrr::reduce(list(R2_alpha_SR, R2_alpha_ENSPIE, R2_gamma_SR, 
+                          R2_gamma_ENSPIE, R2_beta_SR, R2_beta_ENSPIE), 
+              full_join, by = "Effect") 
 
-R2_part_all <- R2_alpha_SR %>%
-  full_join(R2_alpha_ENSPIE, by = "Effect") %>%
-  full_join(R2_gamma_SR, by = "Effect") %>%
-  full_join(R2_gamma_ENSPIE, by = "Effect") %>%
-  full_join(R2_beta_SR, by = "Effect") %>%
-  full_join(R2_beta_ENSPIE, by = "Effect") %>%
+# Filter and reorder factors for plotting and add categories to the drivers
+R2_part_all <- R2_part_all %>%
   filter(!Effect == "Model") %>%
-  mutate(Effect = fct_relevel(Effect, c("poly(pca1_clima, 2)1",
+  mutate(Effect = fct_relevel(Effect, c(
+    "poly(pca1_clima, 2)1",
     "poly(pca1_clima, 2)2",
     "poly(Prec_Varieb, 2)1",
     "poly(Prec_Varieb, 2)2",
@@ -38,7 +39,8 @@ R2_part_all <- R2_alpha_SR %>%
     "mowing1"
   ))) %>%
   arrange(Effect) %>%
-  mutate(Driver = c("Climate gradient",
+  mutate(Driver = c(
+    "Climate gradient",
     "Climate gradient",
     "Precipitation variability",
     "Precipitation variability",
@@ -52,7 +54,8 @@ R2_part_all <- R2_alpha_SR %>%
     "Litter cover",
     "Grazing intencity",
     "Mowing")) %>%
-  mutate(Driver_all = c("Productivity / stress",
+  mutate(Driver_all = c(
+    "Productivity / stress",
     "Productivity / stress",
     "Climate seasonality",
     "Climate seasonality",
@@ -68,13 +71,15 @@ R2_part_all <- R2_alpha_SR %>%
     "Anthropogenic disturbance"))
 
 
-
-R2_1 <- R2_part_all %>%
+# Plot variance explained by driver -------------------------------------------
+# Figure 3 b ------------------------------------------------------------------
+fig_3b_data <- R2_part_all %>%
   group_by(Driver) %>%
-  summarise(alpha_SR = sum(alpha_SR, na.rm = T), alpha_ENSPIE = sum(alpha_ENSPIE, na.rm = T),
-    gamma_SR = sum(gamma_SR, na.rm = T), gamma_ENSPIE = sum(gamma_ENSPIE, na.rm = T),
-    beta_SR = sum(beta_SR, na.rm = T), beta_ENSPIE = sum(beta_ENSPIE, na.rm = T)) %>%
-  mutate(Driver = fct_relevel(Driver, c("Mowing",
+  # sum up all numeric columns by driver
+  summarise(
+    across(where(is.numeric), \(x) sum(x, na.rm = TRUE))) %>%
+  mutate(Driver = fct_relevel(Driver, c(
+    "Mowing",
     "Grazing intencity",
     "Litter cover",
     "Soil pH",
@@ -82,77 +87,70 @@ R2_1 <- R2_part_all %>%
     "Precipitation variability",
     "Climate gradient"
   ))) %>%
-  arrange(Driver) %>%
   pivot_longer(!Driver, names_to = "Diversity", values_to = "R2") %>%
-  mutate(Diversity = fct_relevel(Diversity, c("alpha_SR",
-    "gamma_SR",
-    "beta_SR",
-    "alpha_ENSPIE", "gamma_ENSPIE",
-    "beta_ENSPIE")))
+  # separate the scale and the measure
+  separate_wider_delim(cols = Diversity, delim = "_", 
+                       names = c("scale", "type")) %>%
+  mutate(scale = fct_relevel(scale, c("alpha", "gamma", "beta")),
+         type = fct_relevel(type, c("SR", "ENSPIE")))
 
-R2_1
+fig_3b_data
 
-ggplot(R2_1, # %>% filter(Diversity=="alpha_SR" | Diversity=="gamma_SR" | Diversity=="beta_SR")
-  aes(x = Diversity,
+fig_3b <- ggplot(fig_3b_data, 
+  aes(
+    x = scale,
     y = Driver,
-    fill = Diversity,
+    fill = scale,
     size = R2)) +
   geom_point(pch = 21, colour = "#595959") +
-  #  geom_text(aes(label = round(R2,2)),  colour = "black",  size = 5) +
-  # scale_x_discrete(position = "top") +
+  facet_wrap(~type) +
   scale_size_continuous(range = c(1, 22)) +
-  scale_fill_manual(values = c("alpha_SR" = "#A6CEE3",
-    "alpha_ENSPIE" = "#A6CEE3",
-    "gamma_SR" = "#D6604D",
-    "gamma_ENSPIE" = "#D6604D",
-    "beta_SR" = "#00AC7F",
-    "beta_ENSPIE" = "#00AC7F")) +
-
-  #  scale_color_brewer(palette =  "Paired") +
-  labs(x = NULL, y = NULL, size = R^2 ~ parital) +
+  scale_fill_manual(values = c(
+    "alpha" = "#A6CEE3",
+    "gamma" = "#D6604D",
+    "beta" = "#00AC7F")) +
+  labs(x = "Scale", y = "Biodiversity driver", size = R^2 ~ partial) +
   theme(legend.position = "right",
     legend.key = element_rect(colour = NA, fill = NA),
     panel.background = element_blank(),
     panel.grid = element_line(colour = "grey80"),
     axis.ticks = element_blank(),
-    axis.text.y = element_text(colour = "black", size = 15),
-    axis.text.x = element_text(colour = "black", size = 15)) +
-  guides(color = "none", fill = "none") # remove fill in a legend
+    axis.text = element_text(colour = "black", size = 15)) +
+  guides(color = "none", fill = "none") 
+
+fig_3b
 
 
+# Plot by overall driver ------------------------------------------------------
+# Additional plot that is not part of the paper
 
-## by Driver_all
-
-R2_2 <- R2_part_all %>%
+R2_overall_drivers <- R2_part_all %>%
   group_by(Driver_all) %>%
-  summarise(alpha_SR = sum(alpha_SR, na.rm = T), alpha_ENSPIE = sum(alpha_ENSPIE, na.rm = T),
-    gamma_SR = sum(gamma_SR, na.rm = T), gamma_ENSPIE = sum(gamma_ENSPIE, na.rm = T),
-    beta_SR = sum(beta_SR, na.rm = T), beta_ENSPIE = sum(beta_ENSPIE, na.rm = T)) %>%
+  # sum up all numeric columns by driver
+  summarise(
+    across(where(is.numeric), \(x) sum(x, na.rm = TRUE))) %>%
   mutate(Driver_all = fct_relevel(Driver_all, c("Anthropogenic disturbance",
     "Abiotic disturbance",
     # "Environmental heterogeneity",
     "Productivity / stress"
   ))) %>%
-  arrange(Driver_all) %>%
   pivot_longer(!Driver_all, names_to = "Diversity", values_to = "R2") %>%
-  mutate(Diversity = fct_relevel(Diversity, c("alpha_SR", "alpha_ENSPIE",
-    "beta_SR", "beta_ENSPIE",
-    "gamma_SR", "gamma_ENSPIE")))
-
-R2_2
-
-
-
-
-ggplot(R2_2, aes(x = Diversity,
+  separate_wider_delim(cols = Diversity, delim = "_", 
+                       names = c("scale", "type")) %>%
+  mutate(scale = fct_relevel(scale, c("alpha", "gamma", "beta")),
+         type = fct_relevel(type, c("SR", "ENSPIE")))
+  
+ggplot(R2_overall_drivers, aes(
+  x = scale,
   y = Driver_all,
-  colour = Driver_all, fill = Driver_all,
+  colour = Driver_all, 
+  fill = Driver_all,
   size = R2)) +
   geom_point(pch = 21) +
-  # geom_text(aes(label = round(R2,2)),  colour = "black",  size = 5) +
-  # scale_x_discrete(position = "top") +
   scale_size_continuous(range = c(1, 22)) +
-  scale_fill_manual(values = c("Anthropogenic disturbance" = "#C49A00",
+  facet_wrap(~type) +
+  scale_fill_manual(values = c(
+    "Anthropogenic disturbance" = "#C49A00",
     "Abiotic disturbance" = "#F8CBAD",
     "Productivity / stress" = "#AFD191",
     "Environmental heterogeneity" = "#A6CEE3")) +
@@ -160,13 +158,11 @@ ggplot(R2_2, aes(x = Diversity,
     "Abiotic disturbance" = "#F8CBAD",
     "Productivity / stress" = "#AFD191",
     "Environmental heterogeneity" = "#A6CEE3")) +
-  #  scale_color_brewer(palette =  "Paired") +
-  labs(x = NULL, y = NULL, size = R^2 ~ parital) +
-  theme(legend.position = "right",
+  labs(x = "Scale", y = "Broad drivers", size = R^2 ~ parital) +
+  theme(
     legend.key = element_rect(colour = NA, fill = NA),
     panel.background = element_blank(),
     panel.grid = element_line(colour = "grey80"),
     axis.ticks = element_blank(),
-    axis.text.y = element_text(colour = "black", size = 15),
-    axis.text.x = element_text(colour = "black", size = 15)) +
+    axis.text = element_text(colour = "black", size = 15)) +
   guides(color = "none", fill = "none") # remove fill in a legend
