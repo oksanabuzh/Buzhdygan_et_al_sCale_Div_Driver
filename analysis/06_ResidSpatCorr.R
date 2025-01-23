f10a9a59598567435bfeefe70c162d0388ee5357
Selina Baldauf <selina.baldauf@gmx.de>
2025-01-23 15:01:14 +0100

    Source preparation script
    I double-checked the results and they stay the same

--- analysis/06_ResidSpatCorr.R
+++ analysis/06_ResidSpatCorr.R
@@ -12,93 +12,23 @@ library(lmerTest)
 
 
 # Read and prepare data -------------------------------------------------------
+# This script prepared the following necessary data for all analyses
+# - alpha_data: diversity, ENSPIE, cover and environmental variables for the 10m2 plots
+# - gamma_data: diversity, ENSPIE, cover and environmental variables for the 
+#    100m2 plots
+# - beta_data: diversity, ENSPIE, cover and environmental variables for the
+#    beta scale
+source("analysis/helper_scripts/prepare_data.R")
 
-# Read climate data and compund climate variable from PCA analysis in "1_prepare_data/ PCA_environment.R"
-climate_PCA <- read_csv("data/climate_PCA.csv")
+# Turn mowing variable into a factor
+alpha_data <- mutate(alpha_data, mowing = factor(mowing))
+beta_data <- mutate(beta_data, mowing = factor(mowing))
+gamma_data <- mutate(gamma_data, mowing = factor(mowing))
 
-# Read all environmental data
-header <- read_csv("data/Environm_variabl.csv") %>%
-  full_join(
-    read_csv("data/climate_PCA.csv"),
-    by = "series"
-  )
-
-# mean per series (per 100m2 plots)
-header_mean <- header %>%
-  select(c(series, lat, lon, zonality, habitat_broad,
-           where(is.numeric))) %>%
-  group_by(series, zonality, habitat_broad) %>%
-  summarize(across(where(is.numeric), \(x) mean(x, na.rm = TRUE))) %>%
-  ungroup()
-
-# Prepare subset of data for alpha scale (10 m2 plots) -------------------------
-alpha <- read_csv("data/alpha_beta_gamma_community_variabl.csv") %>%
-  filter(type == "alpha") %>%
-  unite("metric", c(type, scale, metric), sep = "_") %>%
-  pivot_wider(names_from = metric, values_from = value) %>%
-  full_join(header,
-    by = c("dataset", "plotID", "series", "subplot")
-  ) %>%
-  mutate(dataset = factor(dataset))
-
-str(alpha)
-
-# Remove NAs and select only needed variables
-alpha_data <- alpha %>%
-  dplyr::select(alpha_10_div, alpha_10_ENSPIE,
-    lat, lon, pca1_clima,
-    grazing_intencity, mowing,
-    cover_litter,
-    Tem_range, Prec_Varieb,
-    pH, Corg_percent,
-    dataset, series, habitat_broad,
-    subplot) %>%
-  mutate(mowing = factor(mowing)) %>%
-  mutate(habitat = fct_relevel(habitat_broad, c("saline", "complex", "dry",
-    "wet", "mesic", "fringe", "alpine"))) %>%
-  drop_na()
-
-# Prepare subset of data for gamma scale (100 m2 plots) -----------------------
-beta_gamma <- read_csv("data/alpha_beta_gamma_community_variabl.csv") %>%
-  filter(type == "gamma" | type == "beta") %>%
-  unite("metric", c(type, scale, metric), sep = "_") %>%
-  pivot_wider(names_from = metric, values_from = value) %>%
-  full_join(header_mean, by = c("dataset", "series")) %>%
-  mutate(dataset = factor(dataset))
-
-str(beta_gamma)
-
-# Select variables, remove NAs
-gamma_data <- beta_gamma %>%
-  dplyr::select(gamma_100_div, gamma_100_ENSPIE,
-    lat, lon, pca1_clima,
-    grazing_intencity, mowing,
-    # cover_shrub_total,     inclination,
-    cover_litter,
-    Tem_range, Prec_Varieb,
-    pH, Corg_percent,
-    dataset, series, habitat_broad, zonality) %>%
-  mutate(mowing = factor(mowing)) %>%
-  mutate(habitat = fct_relevel(habitat_broad, c("saline", "complex", "dry",
-    "wet", "mesic", "fringe", "alpine"))) %>%
-  drop_na()
-
-# Prepare subset of data for beta scale ---------------------------------------
-beta_data <- beta_gamma %>%
-  dplyr::select(beta_100_div, beta_100_ENSPIE,
-    lat, lon, pca1_clima,
-    grazing_intencity, mowing,
-    # cover_shrub_total,     inclination,
-    cover_litter,
-    Tem_range, Prec_Varieb,
-    pH, Corg_percent,
-    dataset, series, habitat_broad, zonality) %>%
-  mutate(mowing = factor(mowing)) %>%
-  mutate(habitat = fct_relevel(habitat_broad, c("saline", "complex", "dry",
-    "wet", "mesic", "fringe", "alpine"))) %>%
-  drop_na()
-
-str(beta_data)
+# Check how the dataset looks like
+alpha_data
+beta_data
+gamma_data
 
 # Morans's I tests -----------------------------------------------------------
 
